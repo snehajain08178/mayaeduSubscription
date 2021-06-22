c2ae61ad98f0d9a0c35812fbc4e5c5ddcf4df1d1
Vishal Kumar Bhoriwal <vishal.bhoriwal@unthinkable.co>
2021-06-22 13:36:09 +0530

    Update card images

--- src/assets/img/index.js
+++ src/assets/img/index.js
@@ -8,10 +8,23 @@ import mastercardIcon from './mastercard.png';
 import visacardIcon from './visa.png';
 import chipIcon from './chip.png';
 import deleteIcon from './delete.png';
+import americalExpressIcon from './amx.png';
+import discoverIcon from './discover.png';
+import dinersClubIcon from './dinersClub.png';
+import jcbIcon from './jcb.png';
+import visaWhiteIcon from './visaWhite.png';
+import unionPayIcon from './uninonPay.png';
 
 const cardsIcon = {
   master: mastercardIcon,
   visa: visacardIcon,
+  'american-express': americalExpressIcon,
+  discover: discoverIcon,
+  diners: dinersClubIcon,
+  'diners-club': dinersClubIcon,
+  jcb: jcbIcon,
+  unionpay: unionPayIcon,
+  'visa-white': visaWhiteIcon
 };
 
 export default {
